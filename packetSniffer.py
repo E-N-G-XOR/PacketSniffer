3690117ed751b3d2c7746a9fc280d4ba7cc4e79a
ahadsheriff <ahadsheriff@gmail.com>
2016-07-14 18:23:06 -0700

    Unpacked ethernet frame
    Unpack the data coming into your router

--- packetSniffer.py
+++ packetSniffer.py
@@ -2,3 +2,8 @@ import socket
 import struct
 import textwrap
 
+# Unpack ethernet frame
+
+def ethernet_frame(data):
+    reciever_mac, sender_mac, protocol = struct.unpack('! 6s 6s H', data[:14])
+    return getMacAddress(reciever_mac), getMacAddress(sender_mac), socket.htons(socket), data[14:]
