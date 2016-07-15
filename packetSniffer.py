694e6ad66104350fa9707e93d66d9f5efdab588d
ahadsheriff <ahadsheriff@gmail.com>
2016-07-14 20:08:31 -0700

    Return MAC Address
    Will convert the random characters into a properly formatted MAC Address

--- packetSniffer.py
+++ packetSniffer.py
@@ -7,3 +7,72 @@ import textwrap
 def ethernet_frame(data):
     reciever_mac, sender_mac, protocol = struct.unpack('! 6s 6s H', data[:14])
     return getMacAddress(reciever_mac), getMacAddress(sender_mac), socket.htons(socket), data[14:]
+
+# Convert the Mac address from the jumbled up form from above into human readable format
+
+def getMacAddress(bytesAddress):
+    bytesString = map('{:02x}'.format, bytesAddress)
+    macAddress = ':'.join(bytesString).upper()
+    return macAddress
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
