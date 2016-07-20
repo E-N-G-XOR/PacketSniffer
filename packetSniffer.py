6af9046fea389bf5115e37338cd75d7045bf0ed0
ahadsheriff <ahadsheriff@gmail.com>
2016-07-19 18:32:42 -0700

    Unpack ICMP, TCP, and UDP + formatting

--- packetSniffer.py
+++ packetSniffer.py
@@ -2,6 +2,17 @@ import struct
 import textwrap
 import socket
 
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t   '
+DATA_TAB_2 = '\t\t   '
+DATA_TAB_3 = '\t\t\t   '
+DATA_TAB_4 = '\t\t\t\t   '
+
+
 def main():
     connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
@@ -34,6 +45,40 @@ def ip_packet(data):
 #Returns properly formatted IP address
 def ip(address):
     return '.'.join(map(str, address))
+
+#Unpack ICMP packets
+def icmpPackets(data):
+    icmpType, code, checkSum = struct.unpack('! B B H', data[:4])
+    return icmpType, code, checkSum, data[4:]
+
+#Unpack TCP segments:
+def tcpSegment(data):
+    (sourcePort, destinationPort, sequence, acknowledgement, offsetReservedFlags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offsetReservedFlags >> 12) * 4
+
+    flagURG = (offsetReservedFlags & 32) >> 5
+    flagACK = (offsetReservedFlags & 16) >> 4
+    flagPSH = (offsetReservedFlags & 8) >> 3
+    flagRST = (offsetReservedFlags & 4) >> 2
+    flagSYN = (offsetReservedFlags & 2) >> 1
+    flagFIN = offsetReservedFlags & 1
+
+    return sourcePort, destinationPort, sequence, acknowledgement, flagURG, flagACK, flagPSH, flagRST, flagSYN, flagFIN, data[offset:]
+
+#Unpack UDP segments:
+def udpSegment(data):
+    sourcePort, destinationPort, size = struct.unpack('! H H 2x H', data[:8])
+    return sourcePort, destinationPort, size, data[8:]
+
+#Breaks down and formats large, multi-lined data
+def formatMultiLine(prefix, string, size = 80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02X}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 main()
 
